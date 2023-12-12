95182224059bc2d04a1f4b11fd81024a008fd3af
akshaykm002 <akshaykm002@gmail.com>
2023-12-12 16:50:19 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
+
+mongoose.connect(process.env.DATABASE).then(()=>{
+    console.log('Connected to Mongo DB');
+}).catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
 
 app.listen(4000,()=>{
     console.log("Server listening on port 4000 !!!" );
+
 })

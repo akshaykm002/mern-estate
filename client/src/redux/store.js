701d89f660b52bb72aa98181923c3338f2685dda
akshaykm002 <akshaykm002@gmail.com>
2023-12-20 01:41:29 +0530

    redux-persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './User/userSlice'
+
+import {persistReducer} from 'redux-persist'
+
+const rootReducer = combineReducers({user:userReducer})
+
+const persistConfig = {
+    key:'root',
+    storage,
+    version:1
+}
+
+const persistedReducer = persistReducer(persitConfig, rootReducer)
 
 export default configureStore({
   reducer: {

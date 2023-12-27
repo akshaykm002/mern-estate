8ecaf60bd2eed957cfec8e5b5d740c4f01928915
akshaykm002 <akshaykm002@gmail.com>
2023-12-28 00:46:24 +0530

    completed listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -298,7 +298,9 @@ function UpdateListing() {
             />
             <div className='flex flex-col items-center'>
               <p>Regular price</p>
-              <span className='text-xs'>($ / month)</span>
+              {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
             </div>
           </div>
           {formData.offer && (
@@ -315,7 +317,9 @@ function UpdateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Discounted price</p>
-                <span className='text-xs'>($ / month)</span>
+                {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}
               </div>
             </div>
           )}

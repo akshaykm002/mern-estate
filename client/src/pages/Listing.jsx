d7cd19064b59d1666a4934d4a6b18294a03aa41a
akshaykm002 <akshaykm002@gmail.com>
2024-01-04 02:11:01 +0530

    created listing item components and show listings as from search

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -106,7 +106,7 @@ function Listing() {
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ${+listing.regularPrice - +listing.discountPrice}
+                  ${+listing.regularPrice - +listing.discountPrice} OFF
                 </p>
               )}
             </div>
@@ -137,9 +137,12 @@ function Listing() {
               </li>
             </ul>
             {currentUser && listing.userRef !== currentUser._id && !contact && (
-              <button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
-                Contact landlord
-              </button>
+            <button
+            onClick={() => setContact(true)}
+            className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+          >
+            Contact landlord
+          </button>
             )}
             {contact && <Contact listing={listing}/>}
           </div>
